1e80ac46f5512a9bdd210fe4476a85a902723a57
Azazzoo <2295082@cegepoutaouais.qc.ca>
2023-02-16 12:57:34 -0500

    ajout html et ccs source fred, et debut de jQwery

--- JS.js
+++ JS.js
@@ -1,10 +1,41 @@
 $(document).ready(function (){
-    let desc = location.search.substring(1);
-    let descData = desc.split('description=')[1];
-    let dataDesc = descData.split('&')[0]
-    $('#postit').append('<div id="postit"> <p>' + dataDesc + '</p> </div>');
+    // Gestionnaire d'évèvement du boutton soumettre
+    $("form").submit(function (event){
+        // Valider a completer
+        $description = $("description").val();
+        if ($description === ''){
+            event.preventDefault(); // Empeche la soumission
+        }
+
+    });
+//au chargement, vérifier la session
+    $compteur = sessionStorage.getItem("compteur")
+    if ($compteur > 0){
+        for ($i = 0; $i < $compteur; $i++){
+            $ident = "item"+(+$i + 1);
+            $item = sessionStorage.getItem($ident);
+            if($item !== null){
+                $("#fridge").append("<p id="+$ident+">"+$item+"</p>");
+            }
+        }
+    }
+
+    // au chargement, vérifier les données de l'URL
+    $urldata = location.search.substring(1);
+    if($urldata !== "" && $urldata.split('&')[0].split('=')[0] === "description"){
+
+        $desc = $urldata.split('&')[0].split('=')[1];
+
+        // ajouter les items à la session pour les conserver
+        $compteur = +$compteur + 1;
+        sessionStorage.setItem("compteur", $compteur);
+        sessionStorage.setItem("item"+$compteur, $desc);
+
+        // ajouter la description sur un post-it
+        $ident = "item"+$compteur;
+        $("#fridge").append("<p id="+$ident+">"+$desc+"</p>");
+
+    }
 
 
-
-
-})
+});
