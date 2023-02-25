b6f13efcdbd0dfb04663cdda8bc8a203a132d71e
Azazzoo <2295082@cegepoutaouais.qc.ca>
2023-02-25 16:40:04 -0500

    ajout html et ccs source fred, et debut de jQwery

--- JS.js
+++ JS.js
@@ -8,6 +8,74 @@ $(document).ready(function (){
         }
 
     });
+    $descValide = false;
+    $portionValide = false;
+    $idValide = false;
+
+    $("#description").keyup(function (){
+        //Valider la description
+        $desc = $("#description").val();
+        if($desc.length > 100 || $desc.length < 20) {
+            $descValide = false;
+        }
+        else {
+            $descValide = true;
+        }
+        GererBouton();
+    });
+
+    $("#portions").blur(function (){
+        //Valider portions
+        $portions = $("#portions").val();
+        $categorie = $("#cat").val();
+        $min = 2;
+        if ($categorie === "dessert"){
+            $min = 1;
+        }
+        if ($portions < $min || $portions > 8){
+            $portionValide = false;
+        }
+        else {
+            $portionValide = true;
+        }
+        GererBouton();
+    });
+
+    $("#identifiant").blur(function () {
+        //Valider identifiant
+        $identifiant = $("#identifiant").val();
+        $regex = new RegExp("^#[a-z]{4,10}[0-9]{4}$");
+        // Valider l'identifiant à l'aide d'une expression régulière
+        if ($regex.test($identifiant)) {
+            $idValide = true;
+        } else {
+            $idValide = false;
+        }
+        GererBouton();
+    });
+
+    //Décider si on active ou désactive le bouton soumettre
+    function GererBouton() {
+        if($descValide && $portionValide && $idValide){
+            $("#envoyer").removeAttr("disabled");
+        }
+        else {
+            $("#envoyer").attr("disabled", "disabled");
+        }
+    }
+
+
+
+    // gestionnaire d'évènement du bouton
+    $("#envoyer").click(function () {
+        // récupérer les données (description) du formulaire :
+        $desc = $("#description").val();
+        // afficher les post-it
+        $("#fridge").append("<p>" + $desc + "</p>");
+
+
+
+    })
 //au chargement, vérifier la session
     $compteur = sessionStorage.getItem("compteur")
     if ($compteur > 0){
@@ -24,7 +92,8 @@ $(document).ready(function (){
     $urldata = location.search.substring(1);
     if($urldata !== "" && $urldata.split('&')[0].split('=')[0] === "description"){
 
-        $desc = $urldata.split('&')[0].split('=')[1];
+        $desc = ($urldata.split('&')[0].split('=')[1]).replaceAll("+", " ");
+
 
         // ajouter les items à la session pour les conserver
         $compteur = +$compteur + 1;
@@ -34,7 +103,6 @@ $(document).ready(function (){
         // ajouter la description sur un post-it
         $ident = "item"+$compteur;
         $("#fridge").append("<p id="+$ident+">"+$desc+"</p>");
-
     }
 
 
